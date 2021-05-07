044bece288ae8e5a7b817dab1b68d3a40e127a4b
haoliang <haoliang@outlook.jp>
2021-05-07 11:20:09 +0800

    修改 HttpClient 相关代码实现方式

--- src/Support/HttpClient.php
+++ src/Support/HttpClient.php
@@ -2,55 +2,45 @@
 
 namespace Anan\Oss\Support;
 
-use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Utils;
 use Illuminate\Support\Str;
+use Anan\Oss\Traits\OssTrait;
 
 class HttpClient
 {
-    private Client $client;
 
-    public function __construct()
-    {
-
-        $this->client = new Client([
-            // Base URI is used with relative requests
-            'base_uri' => 'https://vision-image.oss-cn-shanghai.aliyuncs.com/',
-            // You can set any number of default request options.
-            'timeout'  => 3.0,
-        ]);
-    }
-
+    use OssTrait;
 
     /**
-     * @throws \Exception|\GuzzleHttp\Exception\GuzzleException
+     * @throws \Exception|GuzzleException
      */
     public function put(string $filePath, String $fileName = ''): string
     {
         $file = new \SplFileInfo($filePath);
-        $util = new Util();
 
         $headers = [
             'User-Agent' => 'QyHttp',
             'Content-Type' => MimeTypes::get($file->getExtension()),
             'Date' => gmdate('D, d M Y H:i:s \G\M\T'),
-            'Host' => $util->getOssHost(),
+            'Host' => $this->getOssHost(),
             'Authorization' => 'OSS '.config('alioss.accessKeyId').':ffMmsvnEJHeUmzQVGoVJsAweWHs='
         ];
 
         $fileName = empty($fileName) ? Str::random(10). '.' . $file->getExtension() : $fileName;
 
-        $headers['Authorization'] = $util->getAuthorization($headers, $fileName);
+        $headers['Authorization'] = $this->getAuthorization($headers, $fileName);
 
-        $result = $this->client->put('activity/' . $fileName, [
+        $result = $this->getHttpClient()->put('activity/' . $fileName, [
             'body' =>  Utils::tryFopen($filePath, 'r'),
             'headers' => $headers
         ]);
 
         if ($result->getStatusCode() == 200) {
-            return $util->getOssHost(true) . "/activity/$fileName";
+            return $this->getOssHost(true) . "/activity/$fileName";
         } else {
             throw new \Exception('Oss 上传失败');
         }
     }
+
 }
